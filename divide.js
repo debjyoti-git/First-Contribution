97c41d9aba34a674e5eb226b2e276405fa931824
debjyoti <debajyoti.bose@harnstech.com>
2022-05-24 11:25:22 +0530

    divide newq feature

--- divide.js
+++ divide.js
@@ -1,3 +1,4 @@
 let a = 5;
 let b = 10;
-console.log("🚀 ~ file: multiply.js ~ line 3 ~ b", a % b);
+console.log("🚀 ~ file: multiply.js ~ line 3 ~ b", a / b);
+console.log("whats app i am adding console log");
